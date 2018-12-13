98a3c5a0186b957cddae6afdbb1d3153ce3550e5
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-12-12 16:44:14 -0800

    did conditional showing of recent submission component

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -1,23 +1,33 @@
 import React from "react";
 import "./FinalPoem.css";
 import PropTypes from "prop-types";
-
+// https://reactjs.org/docs/conditional-rendering.html
+// Reveals entire poem
+// Pass poem lines from Game via props
+// Need method that updates what is returned ?
 const FinalPoem = props => {
-  // Reveals entire poem
-  // Pass poem lines from Game via props
-  // Need method that updates what is returned ?
-  const revealPoem = () => {
-    const poem = props.poem;
-    console.log(poem);
-    //
-  };
+  const poem = props.poem;
+  const showForm = props.revealPoemValue;
+  const showPoemLines = props.poem.map(line => {
+    return <p>{line}</p>;
+  });
+  function poemToShow() {
+    if (showForm) {
+      return (
+        <section className="FinalPoem__poem">
+          <h3>Final Poem</h3>
+          {showPoemLines}
+        </section>
+      );
+    } else {
+      return null;
+    }
+  }
+
   return (
     <div className="FinalPoem">
-      <section className="FinalPoem__poem">
-        <h3>Final Poem</h3>
-      </section>
-
       <div className="FinalPoem__reveal-btn-container">
+        {poemToShow()}
         <input
           type="button"
           value="We are finished: Reveal the Poem"
--- src/components/Game.js
+++ src/components/Game.js
@@ -10,9 +10,19 @@ class Game extends Component {
     super(props);
     this.state = {
       // Add lines of poem to this as they are submitted
-      poemLines: []
+      poemLines: [],
+      revealFinalPoem: false
     };
   }
+
+  showRecentSubmission = () => {
+    if (this.state.poemLines.length > 0) {
+      //return component
+      return <RecentSubmission poem={this.state.poemLines} />;
+    } else {
+      return null;
+    }
+  };
   // Add poem line to poemLines
   addPoemLine = lineToAdd => {
     // Need to make poem
@@ -21,11 +31,14 @@ class Game extends Component {
     this.setState({
       poemLines: this.state.poemLines
     });
-    console.log(this.state.poemLines);
   };
 
   revealPoem = () => {
+    this.setState({
+      revealFinalPoem: true
+    });
     // hide other components
+    console.log(this.state);
     // loop through poem to create paragraph tags under h3 of final poem
   };
 
@@ -41,7 +54,6 @@ class Game extends Component {
     return (
       <div className="Game">
         <h2>Game</h2>
-
         <p>
           Each player should take turns filling out and submitting the form
           below. Each turn should be done individually and <em>in secret!</em>{" "}
@@ -49,20 +61,16 @@ class Game extends Component {
           are finished, click the final button on the bottom to reveal the
           entire poem.
         </p>
-
         <p>Please follow the following format for your poetry submission:</p>
-
         <p className="Game__format-example">{exampleFormat}</p>
-
-        <RecentSubmission poem={this.state.poemLines} />
-
+        {this.showRecentSubmission()}
         <PlayerSubmissionForm
           addPoemLineCallback={this.addPoemLine}
           playerNumber={this.state.poemLines.length + 1}
         />
-
         <FinalPoem
           poem={this.state.poemLines}
+          revealPoemValue={this.state.revealFinalPoem}
           revealPoemCallback={this.revealPoem}
         />
       </div>
